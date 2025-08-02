bf409ed5bebe190da5f681f5847c487b41bcfd58
stana <YOUR_EMAIL>
2025-08-02 21:39:07 +0200

    Implement swap which receives 2 persons and returns them in     the reverse order

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,15 +1,2 @@
-interface User {
-    type: 'user';
-    name: string;
-    age: number;
-    occupation: string;
-}
-interface Admin {
-    type: 'admin';
-    name: string;
-    age: number;
-    role: string;
-}
-export type Person = User | Admin;
 export {};
 //# sourceMappingURL=index.d.ts.map
